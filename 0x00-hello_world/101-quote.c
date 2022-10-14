ca3fb5e57d1f4dbb958faaf5e599e0e767997526
Byron Chigbo <chigbobi@gmail.com>
2022-10-13 18:27:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h> 
-#include <stdio.h> 
+#include <unistd.h> 
 /**
  * main - Entry
  * Return: 1
@@ -8,5 +8,5 @@ int main(void)
 {
 	write(1, "and that piece of art is useful\"
  - Dora Korpar, 2015-10-19\n", 59);
-	 return (1);
-}	
+	return (1);
+}
