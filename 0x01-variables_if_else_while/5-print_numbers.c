15abb61c99f4be839f0aa8b56f5d9aa80ab3cba8
Byron Chigbo <chigbobi@gmail.com>
2022-10-18 05:50:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: prints all strings from 0 to 9
+ * Return: success
+ */
+int main(void)
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
