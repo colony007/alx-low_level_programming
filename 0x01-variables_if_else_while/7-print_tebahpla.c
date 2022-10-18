330c42e20a8e6a914400cf0f29174183329abdc1
Byron Chigbo <chigbobi@gmail.com>
2022-10-18 08:26:41 -0700

    printing alphabet in reverse order

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,12 +4,12 @@
  * description
  * Return: success (0)
  */
- int main(void)
- {
- 	char ch;
+int main(void)
+{
+	char ch;
 
 	for (ch = 'z'; ch <= 'a'; ch--)
 	putchar(ch);
 	putchar('\n');
 	return (0);
- }
+}
