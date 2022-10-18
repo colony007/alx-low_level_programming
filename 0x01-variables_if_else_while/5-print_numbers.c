ec4e05bedf1bf6aaa0661214330dbef7f486a0da
Byron Chigbo <chigbobi@gmail.com>
2022-10-18 06:53:14 -0700

    printing of base 10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,12 +6,9 @@
  * Return: success
  */
 int main(void)
-
+{
 	int i;
-
-	for (int i = 0; i < 10; i++)
-		printf("%d", i);
-
-	printf("\n");
-
+	for (i = 0; i<=10; i++)
+	printf("%d\n", i);
 	return (0);
+}
