10f3b84584335fdb6e5bd18ecd332e01a3132e13
Byron Chigbo <chigbobi@gmail.com>
2022-10-18 07:30:31 -0700

    using putchar to print base 10 number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point of the program
+ * description: Program prints all single digit 
+ * numbers of base 10 starting from 0
+ * Return: success (0)
+ */
+ int main()
+ {
+ 	int a;
+
+	for (a = 0; a < 10; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+ }
