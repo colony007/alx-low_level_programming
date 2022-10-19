cad944cbe2ac7bdc59ef02bd1f772381bbfceb10
Byron Chigbo <chigbobi@gmail.com>
2022-10-19 03:03:28 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,26 +6,19 @@
  */
 int main(void)
 {
-
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
-		putchar(n);
-
-		if (n == 9)
-		continue;
-		
+		putchar(n + '0');
+		if (n < 9)
 		{
 		
-		putchar(',');
-		putchar(' ');
-		
+			putchar(',');
+			putchar(' ');
 		}
 
 	}
-
 	putchar('\n');
-
 	return (0);
 }
