3a39ec85612c2167fd04f28c90773d9680c93e0d
Byron Chigbo <chigbobi@gmail.com>
2022-10-26 17:12:05 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,20 +10,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int b;
-
-	b = 0;
-	while (b < n && src[b] != '\0')
-	{
-		dest[b] = src[b];
-		b++;
-	}
-	while (b < n)
-	{
-		dest[b] = '\0';
-		b++;
-	}
-
+	strncpy(dest, src, n);
 	return (dest);
-
 }
