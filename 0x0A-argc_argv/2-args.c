87d68a8f533a9f6356d58b6ff427a2b3d87426ac
Byron Chigbo <chigbobi@gmail.com>
2022-11-06 01:50:28 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: this is a function to prints its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argv; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
